c4d94608c4b362381de2b6020bad315d104ee524
Om Budhiraja <ombudhiraja144@gmail.com>
2024-07-02 23:54:38 +0530

    handle invalid json gracefully

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDb from './db/connect';
 import indentifyHandler from './handlers/identify';
 import { validateIdentifyRequest } from './middleware/validation';
+import { contacts } from './db/schema';
 
 dotenv.config();
 
@@ -20,13 +21,34 @@ async function main() {
     next();
   });
 
+  // global middleware to handle invalid JSON
+
   app.get('/', async (req, res) => {
     res.json({
       message: 'Access the POST /identify endpoint to identify a contact',
     });
   });
 
+  app.get('/contacts', async (req, res) => {
+    const rows = await req.db.select().from(contacts);
+    res.json(rows);
+  });
+
+  app.get('/identify', async (req, res) => {
+    res.json({
+      message: 'Access the POST /identify endpoint to identify a contact',
+    });
+  });
+
   app.post('/identify', validateIdentifyRequest, indentifyHandler);
+
+  app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
+    if (err instanceof SyntaxError && (err as any).status === 400 && 'body' in err) {
+      return res.status(400).json({ error: 'Invalid JSON' });
+    }
+
+    next();
+  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
